37473f1fb1e577b8c9b07c8d0805c4d1b536d987
alfieAndVim <96734063+alfieAndVim@users.noreply.github.com>
2023-01-28 20:17:54 +0000

    Added support for unit testing

---
+++ GlobalFinance/Client/ServicesInterfaces/IConfiguratorService.cs
@@ -0,0 +1,15 @@
+﻿using GlobalFinance.Shared.Models;
+
+namespace GlobalFinance.Client.ServicesInterfaces
+{
+    public interface IConfiguratorService
+    {
+        ConfigurationModel Configuration { get; set; }
+
+        List<ConfiguratorOptionModel> ConvertModelVariantToOption(List<ModelVariantModel> modelVariants);
+        List<ConfiguratorOptionModel> ConvertPaintToOption(List<PaintModel> paints);
+        double GetFinanceAmount(int totalMonths = 36, int initialPayment = 0, int interestRate = 4);
+        ConfigurationModel GetOrSetConfiguration(CarModel carResult);
+        void SetConfigurationModelVariant(ModelVariantModel modelVariant);
+    }
+}
